cd6160386355d220a13cbbf37d32a2c3f9dea775
Gloridust <Gloridust@gmail.com>
2024-02-10 20:49:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
+# 声明
+print("你正在使用由 Gloridust 制作的 Job-submission-status-Check-tool \n如果你喜欢这个程序，不妨在Github上面start该项目：https://github.com/Gloridust/Job-submission-status-Check-tool \n在使用软件之前，请确保您正确放置了所有文件并正确配置了文件 \n如果有任何问题，你可以在该项目的readme文件或issue中寻求帮助")
+input("按下Enter键开始")
 
 # 配置信息
 excel_name="名单.xlsx"
@@ -6,6 +8,9 @@ name_column="姓名"
 status_column="提交情况"
 file_extensions = ['.doc', '.docx']
 name_is_before="230"
+
+
+import pandas as pd
 
 # 读取Excel文件
 df = pd.read_excel(excel_name)
@@ -39,17 +44,22 @@ for name in name_dic.keys():
         name_dic[name] = 0  # 不存在，保持为未提交
 
 # print(name_dic)
-
+have_sub_num = 0
 have_sub = "已提交的有："
 for name, status in name_dic.items():
     if status == 1:  # 检查值是否为1
+        have_sub_num += 1
         have_sub += (name + ",")  # 记录
+print("已提交人数：",have_sub_num)
 print(have_sub)
 
+not_sub_num = 0
 not_sub = "还未提交的有："
 for name, status in name_dic.items():
     if status == 0:  # 检查值是否为0
+        not_sub_num += 1
         not_sub += (name + ",")  # 记录
+print("未提交人数：",not_sub_num)
 print(not_sub)
 
 # 将提交情况转换为“已交”或“未交”

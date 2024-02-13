451d8c9146a04fee4430614794895e83d8a5f490
Gloridust <Gloridust@gmail.com>
2024-02-13 21:36:02 +0800

    Delete t.py

--- t.py
+++
@@ -1,5 +0,0 @@
-from ltp import LTP
-ltp = LTP()
-word = ltp.pipeline(['秦卓230110105.doc', '秦悦230110130.docx'], tasks=["cws"], return_dict=False)
-word_flat = [item for sublist in word[0] for item in sublist]
-print(word_flat)

dca8b306a3b2b9e0db5e73513a602cbcf7073518
Gloridust <Gloridust@gmail.com>
2024-02-14 12:05:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ excel_name="名单.xlsx"
 name_column="姓名"
 status_column="提交情况"
 file_extensions = ['.doc', '.docx']
-name_is_before="230"
+
 
 import pandas as pd
 import os
@@ -14,7 +14,7 @@ ltp = LTP()
 
 # 声明
 print("你正在使用由 Gloridust 制作的 Job-submission-status-Check-tool \n如果你喜欢这个程序，不妨在Github上面start该项目：https://github.com/Gloridust/Job-submission-status-Check-tool \n在使用软件之前，请确保您正确放置了所有文件并正确配置了文件 \n如果有任何问题，你可以在该项目的readme文件或issue中寻求帮助\n")
-print(f"当前的配置信息如下：\n名单表格：{excel_name}\n姓名所在列：{name_column}\n提交情况列：{status_column}\n提交的文件类型：{file_extensions}\n名字部分前置于：{name_is_before}")
+print(f"当前的配置信息如下：\n名单表格：{excel_name}\n姓名所在列：{name_column}\n提交情况列：{status_column}\n提交的文件类型：{file_extensions}\n")
 input("确认无误后，按下Enter键开始")
 
 # 读取Excel文件
@@ -74,7 +74,7 @@ print(not_sub)
 if work_num == have_sub_num:
     pass
 else:
-    print(">>>注意：检测到的文件数量与识别到的已提交人数不符<<<\n>>>请检查文件命名是否规范<<<")
+    print(">>>注意：检测到的文件数量与识别到的已提交人数不符<<<")
 
 # 将提交情况转换为“已交”或“未交”
 df[status_column] = df[name_column].map(lambda name: '已交' if name_dic[name] == 1 else '未交')
